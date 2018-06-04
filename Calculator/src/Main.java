f2353e3b14023064e2223b29cb8aeac2ad6498f6
Azhar Zaman <azhar.zaman99@gmail.com>
2018-06-04 19:31:04 +0100

    Started work on the Calculator application

---
+++ Calculator/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello, this calculator app is created by Azhar Zaman");
+		Setup start = new Setup();
+	}
+
+}
