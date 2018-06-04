a4b214fd11520eca9895f12d3c8ff103b0f7b499
Azhar Zaman <azhar.zaman99@gmail.com>
2018-06-04 21:00:05 +0100

    Completed the multiplication class

--- Calculator/src/Multiplication.java
+++ Calculator/src/Multiplication.java
@@ -1,4 +1,26 @@
+import java.util.Scanner;
 
 public class Multiplication {
+	
+	int number1;
+	int number2;
+	int answer;
+
+	Scanner input;
+
+	public Multiplication() {
+		
+		input = new Scanner(System.in);
+		
+		System.out.println("Type in the first argument");
+		number1 = input.nextInt();
+		
+		System.out.println("Type in the second argument");
+		number2 = input.nextInt();
+		
+		int answer = number1 * number2;
+		System.out.println("The answer to " + number1 + " x " + number2 + " = " + answer);
+		
+	}
 
 }
