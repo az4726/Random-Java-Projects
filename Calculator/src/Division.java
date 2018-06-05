4d4e3f1c8bba2b7ca0298c0842143250aa2892d9
Azhar Zaman <azhar.zaman99@gmail.com>
2018-06-05 22:40:43 +0100

    Updated the Division class

--- Calculator/src/Division.java
+++ Calculator/src/Division.java
@@ -1,4 +1,26 @@
+import java.util.Scanner;
 
 public class Division {
+	
+	int number1;
+	int number2;
+	int answer;
+
+	Scanner input;
+
+	public Division() {
+		
+		input = new Scanner(System.in);
+		
+		System.out.println("Type in the first argument");
+		number1 = input.nextInt();
+		
+		System.out.println("Type in the second argument");
+		number2 = input.nextInt();
+		
+		int answer = number1 / number2;
+		System.out.println("The answer to " + number1 + " / " + number2 + " = " + answer);
+		
+	}
 
 }
