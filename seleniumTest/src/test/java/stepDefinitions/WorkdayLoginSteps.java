37cc558e12d7c8fbda78588f75ca7513f44e6599
Azhar Zaman <35636613+az4726@users.noreply.github.com>
2025-08-19 20:20:24 +0100

    Correct ChromeDriver options

--- seleniumTest/src/test/java/stepDefinitions/WorkdayLoginSteps.java
+++ seleniumTest/src/test/java/stepDefinitions/WorkdayLoginSteps.java
@@ -7,6 +7,7 @@ import io.cucumber.java.en.When;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import pageObjects.WorkdaySignInPage;
 
@@ -18,8 +19,14 @@ public class WorkdayLoginSteps {
     @Given("I navigate to Workday Login Page")
     public void iNavigateToWorkdayLoginPage() {
         WebDriverManager.chromedriver().setup(); //makes sure you are using chrome
-        webdriver = new ChromeDriver();
-        webdriver.manage().window().maximize();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless"); // run without UI
+        options.addArguments("--disable-gpu"); // required on some environments
+        options.addArguments("--window-size=1920,1080"); // avoid sizing issues
+        options.addArguments("--no-sandbox"); // for Linux CI
+        options.addArguments("--disable-dev-shm-usage"); // for container environments
+
+        webdriver = new ChromeDriver(options);
         webdriver.get("https://auth.api.workday.com/v1/authorize?client_id=YmUwZTVkZjMtMWRjMy00YzE2LTlmM2UtYjBiYjUxMjU1N2Yx&response_type=code&state=9e0bf357b8975cc19906e8e0b9491841fc7e146ef44232ca906904a63db69e4d&redirect_uri=https%3A%2F%2Fdeveloper.workday.com%2Fwelcome&code_challenge_method=S256&code_challenge=VF5uwqtOgFg3ZWKAyTnPVO7DPPZXcjn_cYGK3c5GziI");
         signInPage = new WorkdaySignInPage(webdriver);
     }
