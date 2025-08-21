748a4f5bdf27ea39c3509ad1e40312eb30b8d576
Azhar Zaman <35636613+az4726@users.noreply.github.com>
2025-08-21 21:07:06 +0100

    add environment file, update yaml, correct webdriver for local use, update pom

--- seleniumTest/src/test/java/stepDefinitions/WorkdayLoginSteps.java
+++ seleniumTest/src/test/java/stepDefinitions/WorkdayLoginSteps.java
@@ -5,26 +5,43 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import pageObjects.WorkdaySignInPage;
+import io.github.cdimascio.dotenv.Dotenv;
 
 public class WorkdayLoginSteps {
 
     WebDriver webdriver;
+    Dotenv dotenv = Dotenv.configure().ignoreIfMalformed().load();
     WorkdaySignInPage signInPage;
 
     @Given("I navigate to Workday Login Page")
     public void iNavigateToWorkdayLoginPage() {
         WebDriverManager.chromedriver().setup(); //makes sure you are using chrome
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless"); // run without UI
-        options.addArguments("--disable-gpu"); // required on some environments
-        options.addArguments("--window-size=1920,1080"); // avoid sizing issues
-        options.addArguments("--no-sandbox"); // for Linux CI
-        options.addArguments("--disable-dev-shm-usage"); // for container environments
+
+        if(dotenv.get("PIPELINE_RUN").equals("true")){
+            options.addArguments("--disable-gpu"); // required on some environments
+            options.addArguments("--headless=new"); // run without UI
+            options.addArguments("--window-size=1920,1080"); // avoid sizing issues
+            options.addArguments("--no-sandbox"); // for Linux CI
+            options.addArguments("--disable-dev-shm-usage"); // for container environments
+        }else{
+            options.addArguments("--start-maximized"); //maximised window
+            options.addArguments("--no-sandbox"); // for Linux CI
+            options.addArguments("--safebrowsing-disable-download-protection");
+            options.addArguments("--disable-site-isolation-trials");
+            options.addArguments("--disable-extensions");
+            options.addArguments("--allow-insecure-localhost");
+            options.addArguments("--remote-allow-origins=*");
+            options.addArguments("--deny-permission-prompts");
+            options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+        }
+
 
         webdriver = new ChromeDriver(options);
         webdriver.get("https://auth.api.workday.com/v1/authorize?client_id=YmUwZTVkZjMtMWRjMy00YzE2LTlmM2UtYjBiYjUxMjU1N2Yx&response_type=code&state=9e0bf357b8975cc19906e8e0b9491841fc7e146ef44232ca906904a63db69e4d&redirect_uri=https%3A%2F%2Fdeveloper.workday.com%2Fwelcome&code_challenge_method=S256&code_challenge=VF5uwqtOgFg3ZWKAyTnPVO7DPPZXcjn_cYGK3c5GziI");
